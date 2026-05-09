b7dd586c5823a8ebc26074f4194c2448a0be38f6
Anirudh <141948385+shoppluse@users.noreply.github.com>
2026-05-09 16:58:07 +0530

    Update compressor.py

--- modules/compressor.py
+++ modules/compressor.py
@@ -1,14 +1,27 @@
 from moviepy.editor import VideoFileClip
+import os
 
-def compress_video(input_path, output_path):
+def compress_video(input_path, output_path, quality):
 
     clip = VideoFileClip(input_path)
 
+    bitrate_map = {
+        "Low": "300k",
+        "Medium": "700k",
+        "High": "1200k"
+    }
+
     clip.write_videofile(
         output_path,
-        bitrate="500k",
+        bitrate=bitrate_map[quality],
         codec="libx264",
         audio_codec="aac"
     )
 
     clip.close()
+
+    # FILE SIZE ANALYTICS
+    original_size = os.path.getsize(input_path)
+    compressed_size = os.path.getsize(output_path)
+
+    return original_size, compressed_size
