f578e5b098517643db1d975aeae6e829ffff3a11
Anirudh <141948385+shoppluse@users.noreply.github.com>
2026-05-10 09:51:52 +0530

    Update instagram_downloader.py

--- modules/instagram_downloader.py
+++ modules/instagram_downloader.py
@@ -1,13 +1,25 @@
 import yt_dlp
+import glob
+import os
 
 
-def download_instagram_video(url, output_path):
+def download_instagram_video(url):
+
+    # REMOVE OLD FILES
+    old_files = glob.glob("outputs/instagram_*")
+
+    for file in old_files:
+        os.remove(file)
 
     ydl_opts = {
-        'outtmpl': output_path,
+        'outtmpl': 'outputs/instagram_%(id)s.%(ext)s',
         'format': 'mp4'
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
 
         ydl.download([url])
+
+    files = glob.glob("outputs/instagram_*")
+
+    return files[0]
